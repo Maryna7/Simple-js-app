4f6e81088bfedea9dbede73853805bcc606f9c6e
Maryna7 <m.kurman23@gmail.com>
2023-02-16 11:45:44 +0100

    Delete the previous test task. Create an array in js with a list of objects with the characteristics of Pokemon for the future project.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
-alert('Hello world!');
-
-let favoriteFood = 'Burgers';
-
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Pikachu', height: 0.4, types: ['Field', 'Fairy'], abilities: ['Static', 'Lightningrod']},
+    {name: 'Charmander', height: 0.6, types: ['Monster', 'Dragon'], abilities: ['Blaze', 'Solar-power']},
+    {name: 'Squirtle', height: 0.5, types: ['Monster', 'Water 1'], abilities: ['Rain-dish', 'Torrent']},
+    {name: 'Charmeleon', height: 1.1, types: ['Monster', 'Dragon'], abilities: ['Blaze', 'Solar-power']}
+];
